0aa63f05e933aa030e9799beb8f9cdfd6f1ff07c
nisha <2016ume1288@mnit.ac.in>
2025-07-31 16:45:09 +0530

    fix middleware paths

--- backend/url_shortener/routes.py
+++ backend/url_shortener/routes.py
@@ -27,7 +27,7 @@ async def shorten_url(request:Request,payload:LongUrl=Depends(validate_payload),
 
 @urls_router.post("/shorten/batch")
 async def shorten_url(request:Request,payload:List[LongUrl]=Depends(validate_batch_payload),session_factory=Depends(get_session_factory)): 
-
+    print("shorten")
     user_identifier = request.state.user_identifier
 
     valids_with_idx, failures = payload
